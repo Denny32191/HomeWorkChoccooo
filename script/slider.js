3dfbbff68fbd92867e5bfb2abeb35c31891ff68d
Denis <80045752+Denny32191@users.noreply.github.com>
2023-05-06 00:02:01 +0200

    Add files via upload

--- script/slider.js
+++ script/slider.js
@@ -41,3 +41,50 @@ for (let index = 0; index < buttons.length; index++) {
     })
     
 }
+
+
+//Валидация
+const form = document.querySelector('.form');
+
+const validateField = (field) => {
+    if (field.value.trim().length) {
+        field.classList.remove('form__input_error')
+        return true
+    } else {
+        field.classList.add('form__input_error')
+        return false
+    }
+}
+
+const validateForm = (data) => {
+    let valid = true
+
+    for (const key in data) {
+        const element = data[key];
+
+        const validField = validateField(element)
+        
+        if (!validField) {
+            valid = false
+        }
+    }
+
+    return valid
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    const data = {
+        name: form.elements.name,
+        phone: form.elements.phone,
+        comment: form.elements.comment,
+    }
+
+    if (validateForm(data)) {
+        console.log('форма валидна, отправляем запрос')
+    } else {
+        console.log('запрос не отправляем, ждём заполнения формы')
+    }
+    
+})
