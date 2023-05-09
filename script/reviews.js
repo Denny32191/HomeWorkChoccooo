c68e0b8049c44bc2384afe2ef95568cb1c0dc8e2
Denis <80045752+Denny32191@users.noreply.github.com>
2023-05-09 23:57:18 +0200

    Add files via upload

--- script/reviews.js
+++ script/reviews.js
@@ -12,6 +12,6 @@ $(".interactive-avatar__link").click((e) => {
   const itemToShow = findBlockByAlias(target);
   const curItem = $this.closest(".reviews__switcher-item", ".interactive-avatar");
 
-  itemToShow.addClass("reviews__item--active").siblings().removeClass("reviews__item--active");
+  itemToShow.addClass("reviews__item--active","reviews__item").siblings().removeClass("reviews__item--active");
   curItem.addClass("interactive-avatar--active").siblings().removeClass("interactive-avatar--active");
 });
