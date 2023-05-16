5635b0f23751ca4b9e99ab2a6cb561055854b573
Denis <80045752+Denny32191@users.noreply.github.com>
2023-05-16 15:34:12 +0200

    Add files via upload

--- script/video-player.js
+++ script/video-player.js
@@ -1,111 +1,107 @@
 let player;
-const playerContainer = $(".player");
- 
-let eventsInit = () => {
- $(".player__start").click(e => {
-   e.preventDefault();
- 
-   if (playerContainer.hasClass("paused")) {
-     player.pauseVideo();
-   } else {
-     player.playVideo();
-   }
- });
- 
- $(".player__playback").click(e => {
-   const bar = $(e.currentTarget);
-   const clickedPosition = e.originalEvent.layerX;
-   const newButtonPositionPercent = (clickedPosition / bar.width()) * 100;
-   const newPlaybackPositionSec =
-     (player.getDuration() / 100) * newButtonPositionPercent;
- 
-   $(".player__playback-button").css({
-     left: `${newButtonPositionPercent}%`
-   });
- 
-   player.seekTo(newPlaybackPositionSec);
- });
- 
- $(".player__splash").click(e => {
-   player.playVideo();
- })
-};
- 
+
 const formatTime = timeSec => {
- const roundTime = Math.round(timeSec);
- 
- const minutes = addZero(Math.floor(roundTime / 60));
- const seconds = addZero(roundTime - minutes * 60);
- 
- function addZero(num) {
-   return num < 10 ? `0${num}` : num;
- }
- 
- return `${minutes} : ${seconds}`;
+  const roundTime = Math.round(timeSec);
+
+  const minutes = Math.floor(roundTime / 60);
+  const seconds = roundTime - minutes * 60;
+
+  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  return `${minutes}:${formattedSeconds}`;
 };
- 
+
 const onPlayerReady = () => {
- let interval;
- const durationSec = player.getDuration();
- 
- $(".player__duration-estimate").text(formatTime(durationSec));
- 
- if (typeof interval !== "undefined") {
-   clearInterval(interval);
- }
- 
- interval = setInterval(() => {
-   const completedSec = player.getCurrentTime();
-   const completedPercent = (completedSec / durationSec) * 100;
- 
-   $(".player__playback-button").css({
-     left: `${completedPercent}%`
-   });
- 
-   $(".player__duration-completed").text(formatTime(completedSec));
- }, 1000);
+  let interval;
+  let durationSec = player.getDuration();
+
+  $(".player__duration-estimate").text(formatTime(durationSec));
+
+  if (typeof interval !== "undefined") {
+    clearInterval(interval);
+  }
+
+  interval = setInterval(() => {
+    const completedSec = player.getCurrentTime();
+    const completedPercent = (completedSec / durationSec) * 100;
+
+    $(".player__playback-button").css({
+      left: `${completedPercent}%`
+    });
+
+    $(".player__duration-completed").text(formatTime(completedSec));
+  }, 1000);
 };
- 
+
+const eventsInit = () => {
+  $(".player__start").on("click", e => {
+    e.preventDefault();
+    const btn = $(e.currentTarget);
+
+    if (btn.hasClass("paused")) {
+      player.pauseVideo();
+    } else {
+      player.playVideo();
+    }
+  });
+
+  $(".player__playback").on("click", e => {
+    const bar = $(e.currentTarget);
+    const newButtonPosition = e.pageX - bar.offset().left;
+    const buttonPosPercent = (newButtonPosition / bar.width()) * 100;
+    const newPlayerTimeSec = (player.getDuration() / 100) * buttonPosPercent;
+
+    $(".player__playback-button").css({
+      left: `${buttonPosPercent}%`
+    });
+
+    player.seekTo(newPlayerTimeSec);
+  });
+
+  $(".player__splash").on("click", e => {
+    player.playVideo();
+  });
+};
+
 const onPlayerStateChange = event => {
- /*
-   -1 (воспроизведение видео не начато)
-   0 (воспроизведение видео завершено)
-   1 (воспроизведение)
-   2 (пауза)
-   3 (буферизация)
-   5 (видео подают реплики).
- */
- switch (event.data) {
-   case 1:
-     playerContainer.addClass("active");
-     playerContainer.addClass("paused");
-     break;
- 
-   case 2:
-     playerContainer.removeClass("active");
-     playerContainer.removeClass("paused");
-     break;
- }
+  const playerButton = $(".player__start");
+  /*
+  -1 (воспроизведение видео не начато)
+  0 (воспроизведение видео завершено)
+  1 (воспроизведение)
+  2 (пауза)
+  3 (буферизация)
+  5 (видео подают реплики).
+   */
+  switch (event.data) {
+    case 1: 
+      $('.player__wrapper').addClass('active');
+      playerButton.addClass("paused");
+      break;
+    case 2: 
+      playerButton.removeClass("paused");
+      break;
+  }
 };
- 
+
 function onYouTubeIframeAPIReady() {
- player = new YT.Player("yt-player", {
-   height: "405",
-   width: "660",
-   videoId: "LCeP71B2Mt4",
-   events: {
-     onReady: onPlayerReady,
-     onStateChange: onPlayerStateChange
-   },
-   playerVars: {
-     controls: 0,
-     disablekb: 0,
-     showinfo: 0,
-     rel: 0,
-     autoplay: 0,
-     modestbranding: 0
-   }
- });
+  player = new YT.Player("yt-player", {
+    height: "405",
+    width: "660",
+    videoId: "DNx9hDppHro",
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange
+    },
+    playerVars: {
+      controls: 0,
+      disablekb: 0,
+      showinfo: 0,
+      rel: 0,
+      autoplay: 0,
+      modestbranding: 0
+    }
+  });
 }
- 
+
 eventsInit();
